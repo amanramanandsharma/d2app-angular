6bfe2310ae6c1372c8615ac167e915daf6dfa2cb
Aman Sharma <armslive1904@gmail.com>
2019-02-02 14:56:46 +0530

    fixed game type

--- src/app/pipe/game-type.pipe.ts
+++ src/app/pipe/game-type.pipe.ts
@@ -9,8 +9,12 @@ export class GameTypePipe implements PipeTransform {
     "Unknown","All Pick","Captain’s ,Mode","Random Draft","Single Draft","All Random","Intro","Diretide","Reverse Captain’s Mode","The Greeviling","Tutorial","Mid Only","Least Played","New Player Pool",",Compendium Matchmaking","Custom","Captains Draft",",Balanced Draft","Ability Draft","Event","All Random Death Match","Solo Mid 1 vs 1","Ranked All Pick"
   ];
 
-  transform(value: any ): any {
-    return this.gameType[value];
+  transform(value: any , arg1:any ): any {
+    if(value == 22 && arg1 == 0 ){
+        return "All Pick"
+    }else{
+      return this.gameType[value];
+    }
   }
 
 }
